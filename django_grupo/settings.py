1f2b013befeb59edddaeb6e3f4a99650b033694e
unknown <mariano12martinez@gmail.com>
2025-05-10 11:37:06 -0400

    startapp

--- django_grupo/settings.py
+++ django_grupo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calificaciones.apps.CalificacionesConfig',
+
 ]
 
 MIDDLEWARE = [
