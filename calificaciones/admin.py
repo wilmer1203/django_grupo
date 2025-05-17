66a60123efa5101a1958d70bdf1c11ecf19a11ce
MariaAzuaje <azuajemariac@gmail.com>
2025-05-17 11:20:57 -0400

    Add usuario personalizado

--- calificaciones/admin.py
+++ calificaciones/admin.py
@@ -1,8 +1,10 @@
 # Register your models here.
 
 from django.contrib import admin
-from .models import Estudiante, Curso, Calificacion
+from .models import Estudiante, Curso, Calificacion, UsuarioPersonalizado
+
 
 admin.site.register(Estudiante)
 admin.site.register(Curso)
 admin.site.register(Calificacion)
+admin.site.register(UsuarioPersonalizado)
